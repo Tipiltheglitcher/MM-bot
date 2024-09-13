328d9e98e3c78c1934ce0dd3493abb592a017120
Tipiltheglitcher <144934540+Tipiltheglitcher@users.noreply.github.com>
2024-09-14 01:04:30 +0200

    Create constants.js

---
+++ utils/constants.js
@@ -0,0 +1,19 @@
+module.exports.allowedPlaylists = Object.freeze([
+    "playlist_defaultduo",
+    "playlist_trios",
+    "playlist_defaultsquad",
+    "playlist_nobuildbr_trio",
+    "playlist_nobuildbr_squad",
+    "playlist_nobuildbr_duo",
+    "playlist_blastberrysquad",
+    "playlist_blastberrynobuildsquad",
+    "playlist_blastberryduo",
+    "playlist_blastberrynobuildduo"
+
+])
+
+module.exports.websocketHeaders = Object.freeze({
+    'Accept-Version': '*',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache'
+})
