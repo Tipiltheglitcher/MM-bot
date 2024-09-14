aa4021546e6a7ae97f23cae70ef285831a1fd383
Tipiltheglitcher <144934540+Tipiltheglitcher@users.noreply.github.com>
2024-09-14 12:59:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,18 +14,6 @@ const clientOptions = {
     }
   };
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
 const client = new FNclient(clientOptions);
 party = client.party
 var algorithm = 'aes256';
